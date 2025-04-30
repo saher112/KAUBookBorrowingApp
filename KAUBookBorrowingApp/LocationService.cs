807c3003469d559246ee6e7978562fce637acb48
SAHER SALEM BATRFI <2337157@stu.kau.edu.sa>
2025-05-01 00:32:58 +0300

    new cod #new

--- KAUBookBorrowingApp/LocationService.cs
+++ KAUBookBorrowingApp/LocationService.cs
@@ -17,19 +17,48 @@ public static class LocationService
             using (WebClient wc = new WebClient())
             {
                 string json = wc.DownloadString("http://ip-api.com/json/");
-                GeoInfo location = JsonConvert.DeserializeObject<GeoInfo>(json);
+                GeoInfo userLocation = JsonConvert.DeserializeObject<GeoInfo>(json);
 
+                double userLat = userLocation.lat;
+                double userLon = userLocation.lon;
+
+                // KAU central location
                 double kauLat = 21.4933;
                 double kauLon = 39.2458;
 
-                double distance = GetDistanceInKm(location.lat, location.lon, kauLat, kauLon);
-                return distance <= 5.0;
+                // Get distance in meters
+                double distanceInMeters = GetDistanceInKm(userLat, userLon, kauLat, kauLon) * 1000;
+
+                // ✅ Compare to 6000 meters (6.5 km)
+                return distanceInMeters <= 6200;
             }
         }
         catch
         {
             return false;
         }
+    }
+
+    // Calculates distance in kilometers
+    private static double GetDistanceInKm(double lat1, double lon1, double lat2, double lon2)
+    {
+        const double R = 6371.0; // Radius of the Earth in km
+        double dLat = DegreesToRadians(lat2 - lat1);
+        double dLon = DegreesToRadians(lon2 - lon1);
+
+        double a =
+            Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+            Math.Cos(DegreesToRadians(lat1)) * Math.Cos(DegreesToRadians(lat2)) *
+            Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+
+        double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+        return R * c; // returns distance in kilometers
+    }
+
+    private static double DegreesToRadians(double degrees)
+    {
+        return degrees * (Math.PI / 180);
     }
 
     public static string GetRawLocation()
@@ -43,25 +72,8 @@ public static class LocationService
         }
         catch
         {
-            return "Error retrieving location.";
+            return "Error getting location.";
         }
     }
-
-    private static double GetDistanceInKm(double lat1, double lon1, double lat2, double lon2)
-    {
-        const double R = 6371;
-        double dLat = DegreesToRadians(lat2 - lat1);
-        double dLon = DegreesToRadians(lon2 - lon1);
-        double a =
-            Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
-            Math.Cos(DegreesToRadians(lat1)) * Math.Cos(DegreesToRadians(lat2)) *
-            Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
-        double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
-        return R * c;
-    }
-
-    private static double DegreesToRadians(double deg)
-    {
-        return deg * (Math.PI / 180);
-    }
 }
+
--- KAUBookBorrowingApp/SummaryForm.Designer.cs
+++ KAUBookBorrowingApp/SummaryForm.Designer.cs
@@ -29,30 +29,46 @@
         private void InitializeComponent()
         {
             this.txtSummary = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtSummary
             // 
-            this.txtSummary.BackColor = System.Drawing.Color.Pink;
+            this.txtSummary.BackColor = System.Drawing.SystemColors.Menu;
             this.txtSummary.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSummary.Location = new System.Drawing.Point(79, 95);
+            this.txtSummary.Location = new System.Drawing.Point(185, 154);
+            this.txtSummary.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.txtSummary.Multiline = true;
             this.txtSummary.Name = "txtSummary";
             this.txtSummary.ReadOnly = true;
             this.txtSummary.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtSummary.Size = new System.Drawing.Size(617, 251);
+            this.txtSummary.Size = new System.Drawing.Size(558, 192);
             this.txtSummary.TabIndex = 0;
             this.txtSummary.TextChanged += new System.EventHandler(this.txtSummary_TextChanged);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(124, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "This is final cost";
+            // 
             // SummaryForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(978, 521);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtSummary);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "SummaryForm";
-            this.Text = "SummaryForm";
+            this.Text = "Summary Cost";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -61,5 +77,6 @@
         #endregion
 
         private System.Windows.Forms.TextBox txtSummary;
+        private System.Windows.Forms.Label label1;
     }
 }
